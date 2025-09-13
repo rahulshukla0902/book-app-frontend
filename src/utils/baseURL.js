d0786ce69f3c1d3b1a3fd9c2503688962b23bbcb
Rahul Shukla <rahulshukla0902@gmail.com>
2025-09-13 16:58:06 +0530

    Base URL updated for Dev and Production

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,12 @@
 const getBaseUrl = () => {
-  return "http://localhost:5000";
+  const urls = {
+    development: "http://localhost:5000",
+    production: "https://book-app-backend-rust.vercel.app/",
+  };
+
+  const mode = import.meta.env.MODE; // 'development' or 'production'
+
+  return urls[mode];
 };
 
 export default getBaseUrl;
